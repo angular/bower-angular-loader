5a7dffa240f725bf7129d0ce574383c650c02d11
jenkins <ci-bot@angularjs.org>
2013-12-18 12:41:59 -0800

    v1.2.6-build.2001+sha.3dc1803

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.6-build.2000+sha.73c6671
+ * @license AngularJS v1.2.6-build.2001+sha.3dc1803
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.6-build.2000+sha.73c6671/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.6-build.2001+sha.3dc1803/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
