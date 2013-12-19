dadf1c1409f66f147303028ed40b0b637a040c35
jenkins <ci-bot@angularjs.org>
2013-12-19 05:16:50 -0800

    v1.2.6-build.2011+sha.3d156a7

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.6-build.2010+sha.c7a1d1a
+ * @license AngularJS v1.2.6-build.2011+sha.3d156a7
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.6-build.2010+sha.c7a1d1a/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.6-build.2011+sha.3d156a7/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
