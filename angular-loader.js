13903b8a1a29bb97cd873d68e3fa13278e87291d
jenkins <ci-bot@angularjs.org>
2013-12-27 16:38:07 -0800

    v1.2.7-build.2027+sha.056c849

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2026+sha.7d6e5a2
+ * @license AngularJS v1.2.7-build.2027+sha.056c849
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2026+sha.7d6e5a2/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2027+sha.056c849/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
