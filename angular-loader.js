1fc0ad22bf4daab37acf0b9df39d17c0b29e6fad
jenkins <ci-bot@angularjs.org>
2014-01-02 16:36:34 -0800

    v1.2.7-build.2042+sha.c9705b7

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2041+sha.53ec33f
+ * @license AngularJS v1.2.7-build.2042+sha.c9705b7
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2041+sha.53ec33f/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2042+sha.c9705b7/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
