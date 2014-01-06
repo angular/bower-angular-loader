cb3119457e77ef68b83286ba5334fee042981288
jenkins <ci-bot@angularjs.org>
2014-01-06 12:21:19 -0800

    v1.2.8-build.2077+sha.4c21355

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.8-build.2076+sha.6f6cb5c
+ * @license AngularJS v1.2.8-build.2077+sha.4c21355
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2076+sha.6f6cb5c/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2077+sha.4c21355/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
