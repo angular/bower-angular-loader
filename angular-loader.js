3b8205eb484c6f874c132010ea6c9ed5d7b5a467
jenkins <ci-bot@angularjs.org>
2014-01-13 20:27:50 -0800

    v1.2.9-build.2129+sha.e324c14

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.9-build.2128+sha.e1cfb19
+ * @license AngularJS v1.2.9-build.2129+sha.e324c14
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.9-build.2128+sha.e1cfb19/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.9-build.2129+sha.e324c14/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
