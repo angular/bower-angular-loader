cdfe7690f99b8a2184e1ea799a263f6caf5ff693
jenkins <ci-bot@angularjs.org>
2014-01-15 13:53:51 -0800

    v1.2.10-build.2139+sha.61eb426

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.10-build.2136+sha.8ea8da4
+ * @license AngularJS v1.2.10-build.2139+sha.61eb426
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2136+sha.8ea8da4/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2139+sha.61eb426/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
