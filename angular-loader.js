54330c91013e7895cc29edc37a5931fa6b7a0991
jenkins <ci-bot@angularjs.org>
2014-01-17 13:06:58 -0800

    v1.2.10-build.2150+sha.2262ca6

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.10-build.2149+sha.cd74f74
+ * @license AngularJS v1.2.10-build.2150+sha.2262ca6
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2149+sha.cd74f74/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2150+sha.2262ca6/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
