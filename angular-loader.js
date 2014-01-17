e3fa882f3d084e3acea74281105f6cfbad9f52d0
jenkins <ci-bot@angularjs.org>
2014-01-17 15:30:50 -0800

    v1.2.10-build.2153+sha.8dd4f14

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.10-build.2152+sha.7ba30fd
+ * @license AngularJS v1.2.10-build.2153+sha.8dd4f14
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2152+sha.7ba30fd/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2153+sha.8dd4f14/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
