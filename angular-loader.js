f39ef4c06e90fb2b55db5ce22980f88c795ea7ee
jenkins <ci-bot@angularjs.org>
2014-01-18 13:14:47 -0800

    v1.2.10-build.2158+sha.42ec95e

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.10-build.2157+sha.42ec95e
+ * @license AngularJS v1.2.10-build.2158+sha.42ec95e
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2157+sha.42ec95e/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2158+sha.42ec95e/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
