3468114e68d8dc90587992bd2b330d70a574b8fb
jenkins <ci-bot@angularjs.org>
2014-01-22 12:28:01 -0800

    v1.2.10-build.2176+sha.e020916

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.10-build.2175+sha.c3b5e16
+ * @license AngularJS v1.2.10-build.2176+sha.e020916
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2175+sha.c3b5e16/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2176+sha.e020916/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
