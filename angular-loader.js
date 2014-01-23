986e25abfe28ce17ad21ae0e217f0d9482739792
jenkins <ci-bot@angularjs.org>
2014-01-23 10:01:12 -0800

    v1.2.10-build.2177+sha.5e9835b

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.10-build.2176+sha.e020916
+ * @license AngularJS v1.2.10-build.2177+sha.5e9835b
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2176+sha.e020916/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2177+sha.5e9835b/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
