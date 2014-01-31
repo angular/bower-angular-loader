ebde3f1c1124c9c936770d93ee5cb15206d1a73d
jenkins <ci-bot@angularjs.org>
2014-01-31 10:04:34 -0800

    v1.2.11-build.2198+sha.5ed721b

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.11-build.2197+sha.c22ab5d
+ * @license AngularJS v1.2.11-build.2198+sha.5ed721b
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.11-build.2197+sha.c22ab5d/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.11-build.2198+sha.5ed721b/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
