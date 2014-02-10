8628ee55c1d3875d9090da8f4e95347cb8baba99
jenkins <ci-bot@angularjs.org>
2014-02-10 15:41:39 -0800

    v1.2.13-build.2244+sha.2dfbc08

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.13-build.2243+sha.27613fd
+ * @license AngularJS v1.2.13-build.2244+sha.2dfbc08
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2243+sha.27613fd/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2244+sha.2dfbc08/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
