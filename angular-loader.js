9b2200d880f74672ba081d51ec6dcb9747b0c2f1
jenkins <ci-bot@angularjs.org>
2014-02-12 05:48:53 -0800

    v1.2.13-build.2256+sha.e7ab857

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.13-build.2255+sha.72894f0
+ * @license AngularJS v1.2.13-build.2256+sha.e7ab857
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2255+sha.72894f0/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2256+sha.e7ab857/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
