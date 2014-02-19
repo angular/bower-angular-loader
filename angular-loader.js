3392f525a70b128ed8b37fe24542756dce9d00b8
jenkins <ci-bot@angularjs.org>
2014-02-18 16:44:19 -0800

    v1.2.14-build.2281+sha.a3f7017

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.14-build.2280+sha.868a2c4
+ * @license AngularJS v1.2.14-build.2281+sha.a3f7017
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2280+sha.868a2c4/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2281+sha.a3f7017/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
