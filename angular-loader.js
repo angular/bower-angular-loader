8b691e47c81b392cabe66c428b2b105ba2b3657b
jenkins <ci-bot@angularjs.org>
2014-02-20 12:30:06 -0800

    v1.2.14-build.2296+sha.0478bb4

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.14-build.2295+sha.3193a3a
+ * @license AngularJS v1.2.14-build.2296+sha.0478bb4
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2295+sha.3193a3a/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2296+sha.0478bb4/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
