71165cb5165d5c99f150dfec30fe1aa7399cc7df
jenkins <ci-bot@angularjs.org>
2014-02-25 23:23:38 -0800

    v1.2.14-build.2325+sha.3d6dff4

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.14-build.2324+sha.6b049c7
+ * @license AngularJS v1.2.14-build.2325+sha.3d6dff4
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2324+sha.6b049c7/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2325+sha.3d6dff4/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
