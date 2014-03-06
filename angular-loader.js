1937c7278c91b4b418a368bf8c52dbcb2b9e38a5
jenkins <ci-bot@angularjs.org>
2014-03-06 12:35:18 -0800

    v1.2.15-build.2387+sha.46bd6dc

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.15-build.2386+sha.80451f5
+ * @license AngularJS v1.2.15-build.2387+sha.46bd6dc
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.2386+sha.80451f5/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.2387+sha.46bd6dc/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
