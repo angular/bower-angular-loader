cdd776cdeb213c6ea497397d123dc9342061b15e
jenkins <ci-bot@angularjs.org>
2014-03-20 14:33:36 -0700

    v1.2.15-build.22+sha.320f6d1

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.15-build.21+sha.83f37d7
+ * @license AngularJS v1.2.15-build.22+sha.320f6d1
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.21+sha.83f37d7/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.22+sha.320f6d1/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
