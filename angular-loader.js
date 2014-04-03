cc5c2db13cfed32565f886fcbe172f6ee0b308b6
jenkins <ci-bot@angularjs.org>
2014-04-02 22:16:46 -0700

    v1.2.16-build.75+sha.6da44a4

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2551+sha.ba48797
+ * @license AngularJS v1.2.16-build.75+sha.6da44a4
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2551+sha.ba48797/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.16-build.75+sha.6da44a4/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
