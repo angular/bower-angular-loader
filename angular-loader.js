daaba532f8a417351eefa9dd072836efce08ee49
jenkins <ci-bot@angularjs.org>
2014-04-04 02:30:44 -0700

    v1.2.17-build.87+sha.0f37e49

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.17-build.86+sha.3fdcde7
+ * @license AngularJS v1.2.17-build.87+sha.0f37e49
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.17-build.86+sha.3fdcde7/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.17-build.87+sha.0f37e49/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
