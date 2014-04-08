ffa9fc5010c9df02256b5f0be86cad700fbef06b
jenkins <ci-bot@angularjs.org>
2014-04-08 15:54:13 -0700

    v1.3.0-build.2589+sha.879b0bc

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2588+sha.faec997
+ * @license AngularJS v1.3.0-build.2589+sha.879b0bc
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2588+sha.faec997/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2589+sha.879b0bc/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
