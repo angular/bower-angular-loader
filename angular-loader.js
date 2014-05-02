2495db96d839974ac912ecfb23b0e00b5157f8e4
jenkins <ci-bot@angularjs.org>
2014-05-02 11:06:27 -0700

    v1.3.0-build.2665+sha.924ee6d

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2664+sha.cbc7496
+ * @license AngularJS v1.3.0-build.2665+sha.924ee6d
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2664+sha.cbc7496/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2665+sha.924ee6d/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
