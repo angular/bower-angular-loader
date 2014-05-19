d4990dbf8c2eb850c9d6f03d2e756cd3cf6a556f
jenkins <ci-bot@angularjs.org>
2014-05-19 11:32:24 -0700

    v1.2.17-build.177+sha.bcb53de

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.17-build.176+sha.9d4fa33
+ * @license AngularJS v1.2.17-build.177+sha.bcb53de
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.17-build.176+sha.9d4fa33/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.17-build.177+sha.bcb53de/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
