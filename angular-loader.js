40a5bf558da07da3d47d3968f8cd32be9e17a5ee
jenkins <ci-bot@angularjs.org>
2014-05-21 09:10:36 -0700

    v1.3.0-build.2731+sha.d8c5586

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2730+sha.92f588c
+ * @license AngularJS v1.3.0-build.2731+sha.d8c5586
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2730+sha.92f588c/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2731+sha.d8c5586/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
