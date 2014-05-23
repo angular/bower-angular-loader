36106f0b1ece57f972314deb98028dbdcd2ea14d
jenkins <ci-bot@angularjs.org>
2014-05-23 14:51:30 -0700

    v1.2.17-build.204+sha.ad08638

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.17-build.203+sha.093e76f
+ * @license AngularJS v1.2.17-build.204+sha.ad08638
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.17-build.203+sha.093e76f/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.17-build.204+sha.ad08638/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
