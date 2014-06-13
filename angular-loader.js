bed48bbd4f5750bf57442fa0320537927722ad7e
jenkins <ci-bot@angularjs.org>
2014-06-13 13:43:53 -0700

    v1.2.18-build.248+sha.81b7e5a

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.18-build.247+sha.8661a9e
+ * @license AngularJS v1.2.18-build.248+sha.81b7e5a
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.18-build.247+sha.8661a9e/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.18-build.248+sha.81b7e5a/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
