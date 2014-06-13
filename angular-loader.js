a870a8ddd0e90c656e26feda7c840097c3178e2c
jenkins <ci-bot@angularjs.org>
2014-06-13 14:59:52 -0700

    v1.2.18-build.250+sha.f81d56e

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.18-build.249+sha.f0904cf
+ * @license AngularJS v1.2.18-build.250+sha.f81d56e
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.18-build.249+sha.f0904cf/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.18-build.250+sha.f81d56e/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
