0d3b00e3568e953968df948847b2c7f9860dd1ea
jenkins <ci-bot@angularjs.org>
2014-06-23 11:36:10 -0700

    v1.2.19-build.260+sha.192fecc

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.19-build.259+sha.2a45cea
+ * @license AngularJS v1.2.19-build.260+sha.192fecc
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.19-build.259+sha.2a45cea/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.19-build.260+sha.192fecc/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
