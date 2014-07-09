5e56e76dfafaa7163d00d9fb96575722eed782b8
jenkins <ci-bot@angularjs.org>
2014-07-09 02:54:22 -0700

    v1.2.20-build.292+sha.bee2d1f

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.20-build.291+sha.7101a02
+ * @license AngularJS v1.2.20-build.292+sha.bee2d1f
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.20-build.291+sha.7101a02/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.20-build.292+sha.bee2d1f/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
