10db882a1bb100c92cf37b26cb1a092a21302859
jenkins <ci-bot@angularjs.org>
2014-07-15 06:48:49 -0700

    v1.2.21-build.308+sha.fe01a85

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.21-build.307+sha.fe01a85
+ * @license AngularJS v1.2.21-build.308+sha.fe01a85
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.21-build.307+sha.fe01a85/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.21-build.308+sha.fe01a85/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
