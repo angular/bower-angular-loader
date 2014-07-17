4909a564a2941aa076bbe87e1387bd421598935d
jenkins <ci-bot@angularjs.org>
2014-07-16 17:25:05 -0700

    v1.2.21-build.319+sha.d175bb0

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.21-build.318+sha.25d3d37
+ * @license AngularJS v1.2.21-build.319+sha.d175bb0
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.21-build.318+sha.25d3d37/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.21-build.319+sha.d175bb0/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
