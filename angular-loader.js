7256924074d599fbc1964422a90a6fa1cb565b70
jenkins <ci-bot@angularjs.org>
2014-08-04 04:12:59 -0700

    v1.3.0-build.3021+sha.9475328

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3020+sha.91d5640
+ * @license AngularJS v1.3.0-build.3021+sha.9475328
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3020+sha.91d5640/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3021+sha.9475328/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
