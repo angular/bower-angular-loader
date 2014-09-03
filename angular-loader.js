386dd7d598b6867f9452e66436f775354257b01e
jenkins <ci-bot@angularjs.org>
2014-09-03 03:51:29 -0700

    v1.3.0-build.3178+sha.ac53554

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3177+sha.d9cdb10
+ * @license AngularJS v1.3.0-build.3178+sha.ac53554
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -72,7 +72,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3177+sha.d9cdb10/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3178+sha.ac53554/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
