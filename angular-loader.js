7d541d124c2593feda42522fe4033125006d3250
jenkins <ci-bot@angularjs.org>
2014-09-03 07:09:02 -0700

    v1.3.0-build.3184+sha.821da26

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3181+sha.5b2a386
+ * @license AngularJS v1.3.0-build.3184+sha.821da26
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -72,7 +72,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3181+sha.5b2a386/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3184+sha.821da26/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
