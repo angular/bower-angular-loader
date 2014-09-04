a394cc9d0c30ca5003ef44870619d50514155860
jenkins <ci-bot@angularjs.org>
2014-09-04 06:26:04 -0700

    v1.2.24-build.426+sha.e0cf7c5

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.24-build.425+sha.466320f
+ * @license AngularJS v1.2.24-build.426+sha.e0cf7c5
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.24-build.425+sha.466320f/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.24-build.426+sha.e0cf7c5/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
