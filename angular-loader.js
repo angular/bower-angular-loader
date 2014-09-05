44fa10e770877674c4410f8c553070f5b9fc7088
jenkins <ci-bot@angularjs.org>
2014-09-05 12:16:12 -0700

    v1.2.24-build.437+sha.47f42ec

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.24-build.436+sha.fd995ab
+ * @license AngularJS v1.2.24-build.437+sha.47f42ec
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -69,7 +69,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.24-build.436+sha.fd995ab/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.24-build.437+sha.47f42ec/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
