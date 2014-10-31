64db9e609bd4e18a433429c1196cb8dbac365b96
jenkins <ci-bot@angularjs.org>
2014-10-31 15:20:54 -0700

    v1.3.2-build.3509+sha.2a2fd14

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.2-build.3508+sha.e4eb382
+ * @license AngularJS v1.3.2-build.3509+sha.2a2fd14
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -72,7 +72,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.2-build.3508+sha.e4eb382/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.2-build.3509+sha.2a2fd14/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
