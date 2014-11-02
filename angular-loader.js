4ae5c99810891cae45713ee85ae9664bba0f9fad
jenkins <ci-bot@angularjs.org>
2014-11-02 10:59:11 -0800

    v1.3.2-build.3513+sha.0b16d10

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.2-build.3512+sha.e0198c1
+ * @license AngularJS v1.3.2-build.3513+sha.0b16d10
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -72,7 +72,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.2-build.3512+sha.e0198c1/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.2-build.3513+sha.0b16d10/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
