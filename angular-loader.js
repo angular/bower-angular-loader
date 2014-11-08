7622f9a74adc75b38f62ac398cca6d1fb4ff2150
jenkins <ci-bot@angularjs.org>
2014-11-08 06:08:30 -0800

    v1.3.3-build.3536+sha.50e72fc

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.3-build.3535+sha.b84e62b
+ * @license AngularJS v1.3.3-build.3536+sha.50e72fc
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -72,7 +72,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.3-build.3535+sha.b84e62b/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.3-build.3536+sha.50e72fc/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
