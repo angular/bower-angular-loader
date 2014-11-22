1e0c7a1e95eb0e3a692cf44b7c02f65dbfdecd04
jenkins <ci-bot@angularjs.org>
2014-11-22 12:40:29 -0800

    v1.3.4-build.3603+sha.6dbd606

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.4-build.3602+sha.764fa86
+ * @license AngularJS v1.3.4-build.3603+sha.6dbd606
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.4-build.3602+sha.764fa86/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.4-build.3603+sha.6dbd606/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
