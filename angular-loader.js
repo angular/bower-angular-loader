0cb9295b9e7e61c42e5c81caf89ce3e3d24db190
jenkins <ci-bot@angularjs.org>
2014-12-05 06:35:53 -0800

    v1.3.6-build.3670+sha.2d6a0a1

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.6-build.3669+sha.0c4f9fa
+ * @license AngularJS v1.3.6-build.3670+sha.2d6a0a1
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.6-build.3669+sha.0c4f9fa/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.6-build.3670+sha.2d6a0a1/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
