97015eb3f79e6178d764acf75455750d30513aeb
jenkins <ci-bot@angularjs.org>
2014-12-10 12:10:02 -0800

    v1.3.7-build.3694+sha.d162f15

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.7-build.3693+sha.4025883
+ * @license AngularJS v1.3.7-build.3694+sha.d162f15
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.7-build.3693+sha.4025883/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.7-build.3694+sha.d162f15/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
