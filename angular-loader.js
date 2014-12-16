5c2006d120829a2c99fff458aaae19508516edc6
jenkins <ci-bot@angularjs.org>
2014-12-16 10:14:43 -0800

    v1.3.8-build.3703+sha.0b4e150

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.8-build.3702+sha.e091bb7
+ * @license AngularJS v1.3.8-build.3703+sha.0b4e150
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.8-build.3702+sha.e091bb7/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.8-build.3703+sha.0b4e150/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
