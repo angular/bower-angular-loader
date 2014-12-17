0c0e68a66992d613716b172c55a4efd65155cc2b
jenkins <ci-bot@angularjs.org>
2014-12-17 12:38:53 -0800

    v1.3.8-build.3711+sha.7d70dcd

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.8-build.3710+sha.7d70dcd
+ * @license AngularJS v1.3.8-build.3711+sha.7d70dcd
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.8-build.3710+sha.7d70dcd/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.8-build.3711+sha.7d70dcd/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
