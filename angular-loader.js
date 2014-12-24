5597a494c074400b1c451e4aa4a5748d14153dea
jenkins <ci-bot@angularjs.org>
2014-12-24 15:44:08 -0800

    v1.3.9-build.5+sha.aee3293

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.9-build.3731+sha.0f9fd2f
+ * @license AngularJS v1.3.9-build.5+sha.aee3293
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3731+sha.0f9fd2f/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.5+sha.aee3293/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
