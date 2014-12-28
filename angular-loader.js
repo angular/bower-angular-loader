facb1f580505b62d69f7cdb079b22c8c08072635
jenkins <ci-bot@angularjs.org>
2014-12-28 12:24:33 -0800

    v1.3.9-build.3736+sha.d224fe8

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.9-build.3735+sha.e9bf93d
+ * @license AngularJS v1.3.9-build.3736+sha.d224fe8
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3735+sha.e9bf93d/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3736+sha.d224fe8/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
