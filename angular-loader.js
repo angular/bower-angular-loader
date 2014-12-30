424f98823ecb7f828332ea062c6475238575456f
jenkins <ci-bot@angularjs.org>
2014-12-30 13:09:56 -0800

    v1.3.9-build.3742+sha.2ff7edf

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.9-build.6+sha.b839f73
+ * @license AngularJS v1.3.9-build.3742+sha.2ff7edf
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.6+sha.b839f73/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3742+sha.2ff7edf/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
