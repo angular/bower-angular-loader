e36afd94eaae1f1299bead0bb25054e556d16068
jenkins <ci-bot@angularjs.org>
2015-01-07 11:39:34 -0800

    v1.3.9-build.3754+sha.ab4b632

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.9-build.3753+sha.ab4b632
+ * @license AngularJS v1.3.9-build.3754+sha.ab4b632
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3753+sha.ab4b632/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3754+sha.ab4b632/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
