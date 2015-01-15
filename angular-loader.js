363328f8fe3f35295d9c75ef679581887c81d4d1
jenkins <ci-bot@angularjs.org>
2015-01-15 06:49:50 -0800

    v1.3.10-build.19+sha.3a799df

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.10-build.18+sha.3a799df
+ * @license AngularJS v1.3.10-build.19+sha.3a799df
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.10-build.18+sha.3a799df/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.10-build.19+sha.3a799df/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
