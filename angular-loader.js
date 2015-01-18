91952c19d2332b40d24b7306fc468aba4304b4b9
jenkins <ci-bot@angularjs.org>
2015-01-18 11:07:22 -0800

    v1.4.0-build.3794+sha.2b8baf7

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.0-build.3793+sha.b8e8c55
+ * @license AngularJS v1.4.0-build.3794+sha.2b8baf7
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -58,7 +58,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.0-build.3793+sha.b8e8c55/' +
+    message += '\nhttp://errors.angularjs.org/1.4.0-build.3794+sha.2b8baf7/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
