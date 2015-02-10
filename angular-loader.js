a7c187c7cb1e9708867b277114fafdcf3119dcf6
jenkins <ci-bot@angularjs.org>
2015-02-10 14:54:42 -0800

    v1.4.0-build.3824+sha.c211e7a

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.0-build.3823+sha.d6eba21
+ * @license AngularJS v1.4.0-build.3824+sha.c211e7a
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -58,7 +58,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.0-build.3823+sha.d6eba21/' +
+    message += '\nhttp://errors.angularjs.org/1.4.0-build.3824+sha.c211e7a/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
