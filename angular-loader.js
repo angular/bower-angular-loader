52973e31ac0e045bf902766c59b6aeb1c258755b
jenkins <ci-bot@angularjs.org>
2015-09-15 07:11:16 -0700

    v1.3.19-build.143+sha.3a6bf0d

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.19-build.142+sha.e201f90
+ * @license AngularJS v1.3.19-build.143+sha.3a6bf0d
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.19-build.142+sha.e201f90/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.19-build.143+sha.3a6bf0d/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
