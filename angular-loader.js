0c6d42660e92d6ec8211241b6b081edae8baf1dd
jenkins <ci-bot@angularjs.org>
2015-09-23 13:46:40 -0700

    v1.3.20-build.150+sha.bce1d8e

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.20-build.149+sha.5a1bc6e
+ * @license AngularJS v1.3.20-build.150+sha.bce1d8e
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -55,7 +55,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.20-build.149+sha.5a1bc6e/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.20-build.150+sha.bce1d8e/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
