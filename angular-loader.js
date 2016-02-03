0b3d25fabdc8e49058a1541aeb276a7581a128b2
jenkins <ci-bot@angularjs.org>
2016-02-03 14:23:15 -0800

    v1.5.0-build.4583+sha.a460ce4

--- angular-loader.js
+++ angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-rc.2
+ * @license AngularJS v1.5.0-build.4583+sha.a460ce4
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -87,7 +87,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-rc.2/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4583+sha.a460ce4/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
